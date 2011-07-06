0a6356cf8d7651af0454a849df561136da202f1d
Matthew Boston <matthew.boston@gmail.com>
2011-07-06 18:49:18 -0400

    append subdomain to uri to get item for that woot subdomain

--- hobbes.rb
+++ hobbes.rb
@@ -4,7 +4,7 @@ require 'nokogiri'
 require 'json'
 
 get '/', :provides => 'json' do
-  doc = scrape_woot
+  doc = scrape_woot('www')
   @item = {:name => get_item_name(doc),
             :price => get_item_price(doc),
             :shipping_options => get_item_shipping_options(doc)}
@@ -12,17 +12,26 @@ get '/', :provides => 'json' do
   @item.to_json
 end
 
-def scrape_woot
-  uri = 'http://www.woot.com'
+get '/:subdomain', :provides => 'json' do |subdomain|
+  doc = scrape_woot(subdomain)
+  @item = {:name => get_item_name(doc),
+            :price => get_item_price(doc),
+            :shipping_options => get_item_shipping_options(doc)}
+
+  @item.to_json
+end
+
+def scrape_woot(subdomain)
+  uri = "http://#{subdomain}.woot.com"
   Nokogiri::HTML(open(uri))
 end
 
 def get_item_name(doc)
-  doc.css('h2.fn').first.content
+  doc.css('h2.fn').first.content rescue 'N/A'
 end
 
 def get_item_price(doc)
-  doc.css('h3.price').first.content
+  doc.css('h3.price').first.content rescue 0.00
 end
 
 def get_item_shipping_options(doc)
